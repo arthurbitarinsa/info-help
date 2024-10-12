e5a1aadcbf278c062e846e122ed0ec92f3708d62
zack10 <tshibzack@gmail.com>
2024-10-12 06:18:30 +0200

    some texts updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -29,13 +29,18 @@ function Home() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (!formData.name || !formData.email || !formData.subject || !formData.message) {
+    if (
+      !formData.name ||
+      !formData.email ||
+      !formData.subject ||
+      !formData.message
+    ) {
       setSubmissionStatus("All fields must be filled.");
       return;
     }
 
     setIsSubmitting(true);
-    setSubmissionStatus(""); 
+    setSubmissionStatus("");
 
     const data = {
       from_name: formData.name,
@@ -45,16 +50,13 @@ function Home() {
     };
 
     emailjs
-      .send(
-        "service_oaq5fhn",  
-        "template_abcb0dc", 
-        data,
-        "b4j_WJ11JcUJkySWD"    
-      )
+      .send("service_oaq5fhn", "template_abcb0dc", data, "b4j_WJ11JcUJkySWD")
       .then(
         (response) => {
           console.log("Message sent successfully : ", response);
-          setSubmissionStatus("Thank you for your message! We will get back to you soon.");
+          setSubmissionStatus(
+            "Thank you for your message! We will get back to you soon."
+          );
           setFormData({
             name: "",
             email: "",
@@ -81,23 +83,25 @@ function Home() {
             <div className="side-carousel">
               <h3 className="informed">Get informed !</h3>
               <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic
-                fuga natus explicabo voluptate. Quasi dignissimos enim quae
-                facilis accusantium odit earum accusamus sit repellendus
-                repellat? Magni cum quis placeat asperiores, eos quod. Voluptas,
-                modi quo consectetur recusandae nulla labore dolorum culpa
-                ullam. Tempora qui adipisci eos hic reprehenderit sequi
-                esse?Lorem ipsum dolor,
+                We are dedicated to helping you navigate the resources available
+                in your area. Whether you’re seeking assistance, free food,
+                shelter, or other essential services, you’ve come to the right
+                place. Our goal is to connect you with the help you need, when
+                you need it. Explore our platform, discover local services, and
+                find the support that can make a difference in your life.
+                Together, we can build a stronger community!
               </p>
             </div>
           </div>
           <div className="section-down">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
-            doloribus aliquam officiis ad soluta aperiam hic rem labore, <br />
-            reiciendis atque. Aliquid ut omnis nam suscipit molestiae excepturi
-            alias ad officiis ipsam, rerum autem similique at labore. <br />
-            Atque doloremque asperiores ratione quo accusamus. Vero voluptatum
-            esse voluptatibus saepe quasi quae cupiditate!
+            <i>
+              In every corner, help is near, A guiding light to calm your fear.
+              With open doors and hearts that care, We find the strength to rise
+              and share. Together, we can all be clear.When life feels hard, and
+              hope is thin, Our doors are open, let’s begin. For in the heart,
+              we find the key, To rise as one, to be set free. With every step,
+              together we stand, A world of kindness, hand in hand.
+            </i>
           </div>
           <div className="hr"></div>
           <article>
@@ -110,7 +114,6 @@ function Home() {
                     src="https://sa1s3optim.patientpop.com/assets/images/provider/photos/2541103.jpg"
                     alt=""
                   />
-                  <p>Lorem ipsum dolor sit amet consectetur</p>
                   <button>
                     <a>
                       <Link to="/medication">
@@ -125,7 +128,6 @@ function Home() {
                     src="https://media.cnn.com/api/v1/images/stellar/prod/231113170330-ultraprocessed-foods-stock-image-restricted.jpg?c=original"
                     alt=""
                   />
-                  <p>Lorem ipsum dolor sit amet consectetur</p>
                   <button>
                     <a>
                       <Link to="/food">
@@ -140,7 +142,6 @@ function Home() {
                     src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0c/fc/9a/b2/appartamento-deluxe-with-balcony-.jpg?w=700&h=-1&s=1"
                     alt=""
                   />
-                  <p>Lorem ipsum dolor sit amet consectetur</p>
                   <button>
                     <a>
                       <Link to="/accomodation">
@@ -155,7 +156,6 @@ function Home() {
                     src="https://www.dresden.de/media/bilder/gesundheit/HYG/HYG_Haendewaschen.jpg"
                     alt=""
                   />
-                  <p>Lorem ipsum dolor sit amet consectetur</p>
                   <button>
                     <a>
                       <Link to="/Hygiene">
@@ -170,7 +170,6 @@ function Home() {
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8TcJEmxVvHEQ2P35-m8ro-WsBLEqtG49o3A&s"
                     alt=""
                   />
-                  <p>Lorem ipsum dolor sit amet </p>
                   <button>
                     <a>
                       <Link to="/legal&safety">
@@ -186,7 +185,6 @@ function Home() {
                     src="https://eslbrains.com/wp-content/uploads/2019/09/how-to-learn-a-language-like-a-hyper-polyglot-473x381.png"
                     alt=""
                   />
-                  <p>Lorem ipsum dolor sit amet </p>
                   <button>
                     <a>
                       <Link to="/legal&safety">
@@ -202,7 +200,6 @@ function Home() {
                     src="https://www.open.edu.au/-/media/blog/2022/02-february/counseller-vs-psychologist.jpg?h=583&iar=0&w=715&rev=512145ae285b477ca8b74b09c9b9784b&hash=DC8EFD2462F9FC1C60EB86BA38F5CC89"
                     alt=""
                   />
-                  <p>Lorem ipsum dolor sit amet </p>
                   <button>
                     <a>
                       <Link to="/legal&safety">
@@ -218,7 +215,6 @@ function Home() {
                     src="https://www.salondeletudiant.ch/wp-content/uploads/2020/10/istock-499343530.jpg"
                     alt=""
                   />
-                  <p>Lorem ipsum dolor sit amet </p>
                   <button>
                     <a>
                       <Link to="/legal&safety">
@@ -234,7 +230,6 @@ function Home() {
                     src="https://www.mim-essay.com/images/blogs/Scholarship%20for%20MS%20in%20USA.jpg"
                     alt=""
                   />
-                  <p>Lorem ipsum dolor sit amet </p>
                   <button>
                     <a>
                       <Link to="/legal&safety">
@@ -250,7 +245,6 @@ function Home() {
                     src="https://www.unicef.org/careers/sites/unicef.org.careers/files/styles/two_column/public/Priscilla%20Idele3.jpg.webp?itok=IYiHvGx2"
                     alt=""
                   />
-                  <p>Lorem ipsum dolor sit amet </p>
                   <button>
                     <a>
                       <Link to="/legal&safety">
@@ -266,7 +260,6 @@ function Home() {
                     src="https://www.departement13.fr/uploads/pics/2024-01-29_BANNER_Tournee_Terre_de_Sports.jpg"
                     alt=""
                   />
-                  <p>Lorem ipsum dolor sit amet </p>
                   <button>
                     <a>
                       <Link to="/legal&safety">
@@ -282,7 +275,6 @@ function Home() {
                     src="https://pbs.twimg.com/profile_images/558750489152458752/pBBTFL0j_400x400.png"
                     alt=""
                   />
-                  <p>Lorem ipsum dolor sit amet </p>
                   <button>
                     <a>
                       <Link to="/legal&safety">
@@ -297,7 +289,7 @@ function Home() {
         </section>
 
         <section className="section2">
-        <div className="hr"></div>
+          <div className="hr"></div>
           <div className="support-container">
             <div className="support-h1">
               <h1>Leave a message</h1>
@@ -347,7 +339,9 @@ function Home() {
                 {isSubmitting ? "Sending..." : "Submit"}
               </button>
             </form>
-            {submissionStatus && <p className="success-message">{submissionStatus}</p>}
+            {submissionStatus && (
+              <p className="success-message">{submissionStatus}</p>
+            )}
           </div>
         </section>
         <Footer />
